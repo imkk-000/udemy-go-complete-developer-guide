cd64ff554ca234b8fd9287f11c242df62c379aee
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2020-05-23 21:29:43 +0700

    Add creating new deck from file

--- cards/main.go
+++ cards/main.go
@@ -1,6 +1,9 @@
 package main
 
 func main() {
-	cards := newDeck()
-	cards.saveToFile("my_cards")
+	oldCards := newDeck()
+	oldCards.saveToFile("my_cards")
+
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
